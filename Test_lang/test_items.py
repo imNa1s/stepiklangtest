598015f8ecbb3cbb481029651759cf6652d5c52b
Na1s <imna1se@gmail.com>
2021-12-09 18:20:49 +0500

    add stuff for checking button "add to basket"

--- Test_lang/test_items.py
+++ Test_lang/test_items.py
@@ -1,6 +1,12 @@
+import time
+from selenium.webdriver.common.by import By
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_diff_language(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.submit")
+    browser.implicitly_wait(10)
+    check = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert len(check) > 0, "can't find button"
+    time.sleep(5)
