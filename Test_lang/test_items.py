cbb6b77402a1be256d73ab4357939b3dcd35697b
Na1s <imna1se@gmail.com>
2021-12-09 12:40:19 +0500

    init

---
+++ Test_lang/test_items.py
@@ -0,0 +1,6 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("button.submit")
